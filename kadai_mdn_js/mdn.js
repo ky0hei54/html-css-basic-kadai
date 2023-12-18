fc378756a9a0d0b75d1b94d72ae39b6569a0c554
ky0hei54 <150123146+ky0hei54@users.noreply.github.com>
2023-12-18 23:10:29 +0900

    kadai_mdn_jsをadd

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+const today =  new Date();
+
+const [year, month, day] = [
+    today.getFullYear(),
+    today.getMonth() +1,
+    today.getDate()
+];
+
+console.log(year + '年', month + '月', day  + '日');
+
+
+
